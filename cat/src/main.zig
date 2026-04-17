c1eeea5d5346af8c02f5fd0f9739f6b8cde78ed1
milkymilky0116 <sjlee990129@gmail.com>
2026-04-17 17:14:33 +0900

    chore: implementing cat

--- cat/src/main.zig
+++ cat/src/main.zig
@@ -1,71 +1,47 @@
 const std = @import("std");
 const Io = std.Io;
+const Writer = std.Io.File.Writer;
 
-const cat = @import("cat");
+const Cli = struct {
+    file_list: []const []const u8,
+    is_version: bool,
+    is_help: bool,
+    is_non_blank_line: bool,
+    is_line: bool,
+
+    pub fn init(args_slice: []const []const u8) Cli {
+        var cli = Cli{
+            .file_list = .{},
+            .is_version = false,
+            .is_help = false,
+            .is_non_blank_line = false,
+            .is_line = false,
+        };
+
+        for (args_slice) |arg| {
+            if (std.mem.eql([]const u8, arg, "-h") or std.mem.eql([]const u8, arg, "--help")) {
+                cli.is_help = true;
+            } else if (std.mem.eql([]const u8, arg, "-v") or std.mem.eql([]const u8, arg, "--version")) {
+                cli.is_version = true;
+            } else if (std.mem.eql([]const u8, arg, "-b") or std.mem.eql([]const u8, arg, "--number--nonblank")) {
+                cli.is_non_blank_line = true;
+            } else if (std.mem.eql([]const u8, arg, "-n") or std.mem.eql([]const u8, arg, "--number")) {
+                cli.is_line = true;
+            } else {}
+        }
+
+        return cli;
+    }
+};
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
-
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
-    }
-
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    const arena = init.arena.allocator();
     var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    var stdout_writer = Writer.init(std.Io.File.stdout(), init.io, &stdout_buffer);
+    var stdout = &stdout_writer.interface;
 
-    try cat.printAnotherMessage(stdout_writer);
+    const cli = Cli.init(init.minimal.args.toSlice(arena));
+    _ = cli;
 
-    try stdout_writer.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
+    try stdout.flush();
 }
